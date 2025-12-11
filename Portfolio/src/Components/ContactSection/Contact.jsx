7a09909828cde1183fd3f6bedc7da9ac255cdd42
Ananya <ananyabera103@gmail.com>
2025-12-12 00:28:23 +0530

    Contact section changed

--- Portfolio/src/Components/ContactSection/Contact.jsx
+++ Portfolio/src/Components/ContactSection/Contact.jsx
@@ -2,14 +2,14 @@ import { ChevronDownIcon } from '@heroicons/react/16/solid'
 
 export default function Contact() {
   return (
-    <div className="isolate gradient px-85 py-9 m-10 rounded-3xl">
+    <div className="isolate bg-rose-50 px-85 py-6 m-10 rounded-3xl shadow">
       <div className=" text-center">
-        <h2 className="text-2xl font-semibold tracking-tight text-rose-50 ">Contacts </h2>
+        <h2 className="text-2xl font-semibold tracking-tight text-rose-500 ">Contacts </h2>
       </div>
       <form action="#" method="POST" className="mx-auto mt-5 flex flex-col justify-center">
         
           <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm/6 font-semibold text-white">
+            <label htmlFor="email" className="block text-sm/6 font-semibold text-violet-600">
               Full Name
             </label>
             <div className="mt-1">
@@ -17,7 +17,8 @@ export default function Contact() {
                 id="fullname"
                 name="fullname"
                 type="text"
-                className="block w-full rounded-md bg-white/5 px-3.5 py-1 text-base text-white outline-1 -outline-offset-1 outline-rose-100 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200"
+                placeholder='Your name'
+                className="block w-full rounded-md bg-purple-100 px-3.5 py-1 text-base text-violet-600 outline-1 -outline-offset-1 outline-purple-600 placeholder:text-stone-400 focus:outline-2 focus:-outline-offset-2 focus:outline-purple-300"
               />
             </div>
           </div>
@@ -25,7 +26,7 @@ export default function Contact() {
           
           
           <div className="sm:col-span-2">
-            <label htmlFor="email" className="block text-sm/6 font-semibold text-white">
+            <label htmlFor="email" className="block text-sm/6 font-semibold text-violet-600">
               Email
             </label>
             <div className="mt-1">
@@ -34,13 +35,14 @@ export default function Contact() {
                 name="email"
                 type="email"
                 autoComplete="email"
-                className="block w-full rounded-md bg-white/5 px-3.5 py-1 text-base text-white outline-1 -outline-offset-1 outline-rose-100 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200"
+                placeholder='example@gmail.com'
+                className="block w-full rounded-md bg-purple-100 px-3.5 py-1 text-base text-violet-600 outline-1 -outline-offset-1 outline-purple-600 placeholder:text-stone-400 focus:outline-2 focus:-outline-offset-2 focus:outline-purple-300"
               />
             </div>
           </div>
           
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block text-sm/6 font-semibold text-white">
+            <label htmlFor="message" className="block text-sm/6 font-semibold text-violet-600">
               Message
             </label>
             <div className="mt-1">
@@ -48,17 +50,17 @@ export default function Contact() {
                 id="message"
                 name="message"
                 rows={2}
-                className="block w-full rounded-md bg-white/5 px-3.5 py-1 text-base text-white outline-1 -outline-offset-1 outline-rose-100 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-rose-200"
+                className="block w-full rounded-md bg-purple-100 px-3.5 py-1 text-base text-violet-600 outline-1 -outline-offset-1 outline-purple-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-purple-300"
                 defaultValue={''}
               />
             </div>
           </div>
           
         
-        <div className="mt-10 flex justify-center">
+        <div className="mt-8 flex justify-center">
           <button
             type="submit"
-            className=" bg-rose-500 text-white text-lg font-semibold rounded-xl py-2 px-10 transition durstion-300 ease-in-outhover:-translate-y-1 hover:scale-110"
+            className=" bg-rose-400 text-white text-lg font-semibold rounded-xl py-2 px-10 transition durstion-300 ease-in-outhover:-translate-y-1 hover:scale-110"
           >
             Let's talk
           </button>
--- Portfolio/src/Footer.jsx
+++ Portfolio/src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='py-5 text-rose-800 text-xm bg-rose-200 text-center' >
+    <div className='py-5 text-rose-800 text-xm bg-rose-200 text-center font-medium' >
       <p>© 2025 PORTFOLIO. All rights reserved.</p>
     </div>
   )
