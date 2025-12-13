ecd958944d20fd17d5e39cfeeee2abcc35de2431
Ananya <ananyabera103@gmail.com>
2025-12-13 21:51:21 +0530

    animation added to homepage

--- Portfolio/src/Components/HomePage/HeroLeft.jsx
+++ Portfolio/src/Components/HomePage/HeroLeft.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroLeft = () => {
   return (
-    <div className='px-10 py-5 text-center w-1/3'>
+    <div className='px-10 py-5 text-center w-1/3' data-aos='slide-down'>
       <div className=' h-80 w-80 border-2 justify-center rounded-full '>Image</div>
     </div>
   )
--- Portfolio/src/Components/HomePage/HeroRight.jsx
+++ Portfolio/src/Components/HomePage/HeroRight.jsx
@@ -6,8 +6,8 @@ const HeroRight = () => {
     <div className='flex flex-row justify-between pl-10 py-5 pr-4 w-2/3 items-center'>
       <div className='flex flex-col justify-center py-10'>
         <h2 className='text-[#9c806e] font-bold text-2xl'>Hi, It's Ananya</h2>
-        <span className='tracking-tight HText font-serif'> <span className='text-[#31231b]'>Front</span>end Developer</span>
-        <button className='btn bg-[#856551] text-[#fff8ef] transition durstion-300 ease-in-outhover:-translate-y-2 hover:scale-120'>Dounload CV</button>
+        <span className='tracking-tight HText font-serif' data-aos='fade-right'> <span className='text-[#31231b]'>Front</span>end Developer</span>
+        <button data-aos='fade-up' className='btn bg-[#856551] text-[#fff8ef] transition durstion-300 ease-in-outhover:-translate-y-2 hover:scale-120'>Dounload CV</button>
       </div>
       <div>
         <Socials />
