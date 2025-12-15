2f4a00cfada446002ffc0f7ec8ba65c51523deb5
Ananya <ananyabera103@gmail.com>
2025-12-15 19:02:47 +0530

    made some changes

--- Portfolio/src/Components/ProjectSection/Projects.jsx
+++ Portfolio/src/Components/ProjectSection/Projects.jsx
@@ -3,8 +3,8 @@ import ProjectGrid from './ProjectGrid'
 
 const Projects = () => {
   return (
-    <div className='h-screen text-center px-10 py-8 font-serif'>
-      <h2 className='text-3xl font-bold text-[#410307]'>My Works</h2>
+    <div className='h-screen text-center px-10 py-8 font-serif bg-[#410307]'>
+      <h2 className='text-3xl font-bold text-amber-50'>My Works</h2>
       <div>
         <ProjectGrid />
       </div>
