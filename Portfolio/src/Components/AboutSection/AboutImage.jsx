5844d32885952125117a029a3158be094b464632
Ananya <ananyabera103@gmail.com>
2025-12-21 23:19:10 +0530

    made some changes

--- Portfolio/src/Components/AboutSection/AboutImage.jsx
+++ Portfolio/src/Components/AboutSection/AboutImage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutImage = () => {
   return (
     <div className='py-10 px-7'>
-    <div className='flex text-center w-[60vh] h-[60vh] bg-[#410307]  '>
+    <div className='flex text-center w-[60vh] h-[60vh] bg-[#410307] text-amber-50 '>
       image
     </div>
     </div>
