d5c79edde18aca914560a0f6b88d90ebbce49bc1
Ananya <ananyabera103@gmail.com>
2025-12-26 22:13:17 +0530

    made some changes

--- Portfolio/src/Navbar.jsx
+++ Portfolio/src/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     
-    <div className='items-center justify-between flex px-6 py-4 text-center bg-[#fff4e4] sticky top-0 z-50'>
+    <div className='items-center justify-between flex px-6 py-4 text-center bg-[#f9f6f2] sticky top-0 z-50'>
       <h1 className='text-[15px] text-[#884841] font-bold'>Ananya.</h1>  
       <div className='gap-5 text-lg font-medium flex items-center cursor-pointer  text-[#884841]'>
       <a className='px-5 py-1 rounded-xl hover:text-[#360005]'>About</a>
